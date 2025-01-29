d0c13b28f19ce2d78e67cac9f306b85c14a1ba69
Waddah Chelly <73547198+waddah98@users.noreply.github.com>
2025-01-29 13:38:03 +0100

    update

--- front/src/app/admin/products/products.component.ts
+++ front/src/app/admin/products/products.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderSidebarComponent } from "../header-sidebar/header-sidebar.component";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [HeaderSidebarComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
