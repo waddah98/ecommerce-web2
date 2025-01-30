a1397bea4ab372ea31363708911b0d2eefc06789
Waddah Chelly <73547198+waddah98@users.noreply.github.com>
2025-01-30 17:32:31 +0100

    update

--- front/src/app/admin/home-admin/home-admin.component.ts
+++ front/src/app/admin/home-admin/home-admin.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
-import { HeaderSidebarComponent } from "../header-sidebar/header-sidebar.component";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home-admin',
   standalone: true,
-  imports: [HeaderSidebarComponent],
+  imports: [
+    MatToolbarModule,
+    MatCardModule,
+    RouterLink,
+  ],
   templateUrl: './home-admin.component.html',
   styleUrl: './home-admin.component.scss'
 })
 export class HomeAdminComponent {
+
+  menuItems = [
+    {
+      name: "Categories",
+      icon: "bi bi-tags-fill",
+      link: "/admin/categories"
+    },
+    {
+      name: "Products",
+      icon: "bi bi-shop",
+      link: "/admin/products"
+    },
+    {
+      name: "Users",
+      icon: "bi bi-people-fill",
+      link: "/admin/users"
+    },
+    
+  ];
 
 }
--- front/src/app/admin/products/products.component.ts
+++ front/src/app/admin/products/products.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { HeaderSidebarComponent } from "../header-sidebar/header-sidebar.component";
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [HeaderSidebarComponent],
+  imports: [],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
