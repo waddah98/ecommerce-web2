702f688c6d825647affb04cefaf6b7d3d6545dbe
Waddah Chelly <73547198+waddah98@users.noreply.github.com>
2025-02-07 16:22:00 +0100

    update

--- front/src/app/client/client.component.ts
+++ front/src/app/client/client.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { GalleriaModule } from 'primeng/galleria';
+import { PhotoService } from '@/service/photoservice';
 
 
 @Component({
@@ -12,9 +13,28 @@ import { GalleriaModule } from 'primeng/galleria';
     MatToolbarModule,
     GalleriaModule,
   ],
+  providers: [PhotoService],
   templateUrl: './client.component.html',
   styleUrl: './client.component.scss'
 })
 export class ClientComponent {
+  images = model([]);
+
+  responsiveOptions: any[] = [
+    {
+        breakpoint: '1300px',
+        numVisible: 4
+    },
+    {
+        breakpoint: '575px',
+        numVisible: 1
+    }
+];
+
+constructor(private photoService: PhotoService) {}
+
+    ngOnInit() {
+        this.photoService.getImages().then((images: never[]) => this.images.set(images));
+    }
 
 }
